b5ca0b2b8675befa13fa57ad8e96f2721337de21
Lucas Wall <lucas.wall@lemonchiligames.com>
2016-08-28 22:18:07 -0300

    Center word spaces.

--- Assets/Sources/WordContainer.cs
+++ Assets/Sources/WordContainer.cs
@@ -8,6 +8,7 @@ public class WordContainer : MonoBehaviour {
 	public Vector3 spawnSpacing;
 
 	public void SetupSpaces(WordData wordData) {
+		startPos.x = - spawnSpacing.x * ( wordData.word.Length - 1 ) / 2;
 		for ( int i = 0; i < wordData.word.Length; i++ ) {
 			LetterSpace space = Instantiate<LetterSpace>(letterSpacePrefab);
 			space.transform.SetParent(transform);
